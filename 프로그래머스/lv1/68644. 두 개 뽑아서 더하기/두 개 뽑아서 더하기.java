64772831254f07f9016a87a122f4b3d0be05dccf
ChaedongIm <chaedong.im.dev@gmail.com>
2023-03-17 22:23:40 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    // 이중 포문으로 다 더하면서 Set에 넣기
+    // Set에서 list로 빼고 정렬하기
+    public int[] solution(int[] numbers) {
+        int len = numbers.length;
+        Set<Integer> set = new HashSet();
+        
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        int[] answer = set.stream().mapToInt(x->x).toArray();
+        Arrays.sort(answer);
+        return answer;
+    }
+}
