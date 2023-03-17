08be4d51b3e246b3904c265b8b2ac5929fda55ad
ChaedongIm <chaedong.im.dev@gmail.com>
2023-03-17 18:03:06 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.79 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 0.08 ms
+메모리: 79 MB, 시간: 3.79 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr) {
         if (arr.length <= 1) {
@@ -5,29 +7,8 @@ class Solution {
             return answer;
         }
         
-        int min = getMinOfArray(arr);
-        
-        int[] answer = new int[arr.length - 1];
-        int j = 0;
-        boolean hasNotMeet = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (hasNotMeet && arr[i] == min) {
-                hasNotMeet = false;
-                continue;
-            }
-            
-            answer[j++] = arr[i];
-        }
-        
-        return answer;
-    }
-    
-    private int getMinOfArray(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            min = Math.min(min, arr[i]);
-        }
-        
-        return min;
+        int[] answer = new int[0];
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(x->x != min).toArray();
     }
 }
