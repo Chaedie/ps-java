bd064beacf7958add1478fe07029c56ee3f9353f
ChaedongIm <chaedong.im.dev@gmail.com>
2023-03-24 10:45:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.42 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,13 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int countSame = 0;
         int countZero = 0;
+        
+        Arrays.sort(win_nums);
         for (int num: lottos) {
             if (num == 0) {
                 countZero++;
-            } else if (hasSameNumber(win_nums, num)){
+            } else if (Arrays.binarySearch(win_nums, num) >= 0) {
                 countSame++;
             }
+         
         }
 
         int[] result = {6,6,5,4,3,2,1};
@@ -15,12 +20,17 @@ class Solution {
         return answer;
     }
     
-    private boolean hasSameNumber(int[] win_nums, int target){
-        for (int winNum: win_nums) {
-            if (winNum == target) {
-                return true;
-            }
-        }
-        return false;
-    }
+    
+   // } else if (hasSameNumber(win_nums, num)){
+        //     countSame++;
+        // }
+
+    // private boolean hasSameNumber(int[] win_nums, int target){
+    //     for (int winNum: win_nums) {
+    //         if (winNum == target) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
