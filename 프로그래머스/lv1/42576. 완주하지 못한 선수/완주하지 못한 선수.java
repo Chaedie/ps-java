a6eb208cd10f9c87ce80c2e62a9e79d200879e44
ChaedongIm <chaedong.im.dev@gmail.com>
2023-03-31 11:00:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.43 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        // 두 배열 정렬함  
-        // 정렬했는데 순회하다가 두개가 다른값이 나오면 그 값 리턴
+     public String solution(String[] participant, String[] completion) {
+        // map 두개 만들어서
+        // 두개 비교하면서 count값 다르면 그놈 return
 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for (int i = 0; i < participant.length-1; i++) {
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
+        Map<String, Integer> partMap = new HashMap<>();
+
+        for (String name : participant) {
+            Integer count = partMap.getOrDefault(name, 0);
+            partMap.put(name, count + 1);
+        }
+        for (String name : completion) {
+            Integer count = partMap.getOrDefault(name, 0);
+            partMap.put(name, count - 1);
+        }
+
+        for (String name : participant) {
+            if (partMap.get(name) != 0) {
+                return name;
             }
         }
 
-        return participant[participant.length - 1];
+        return "";
     }
 }
+
+// public String solution(String[] participant, String[] completion) {
+//     // 두 배열 정렬함  
+//     // 정렬했는데 순회하다가 두개가 다른값이 나오면 그 값 리턴
+
+//     Arrays.sort(participant);
+//     Arrays.sort(completion);
+//     for (int i = 0; i < participant.length-1; i++) {
+//         if (!participant[i].equals(completion[i])) {
+//             return participant[i];
+//         }
+//     }
+
+//     return participant[participant.length - 1];
+// }
