9b1f4c3331c8f59bca3836774e387ac8da06fe23
ChaedongIm <chaedong.im.dev@gmail.com>
2023-04-05 20:49:41 +0900

    [level 2] Title: 위장, Time: 0.10 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    //map에 <종류, 갯수> <String, Integer>로 담는다.
+    // 종류를 순회하면서 갯수를 다 곱해주면 경우의수
+    // 카운트 + 1 끼리 곱하고, 아무것도 안입는 경우의 수 1을 빼주면 됨
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] strs: clothes) {
+            map.put(strs[1], map.getOrDefault(strs[1], 0) + 1);
+        }
+        
+        int answer = 1;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= map.get(entry.getKey()) + 1;
+        }
+        
+        return answer - 1;
+    }
+}
