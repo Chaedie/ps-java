95ee376c5538bca1abd4f9566645128eed1241d8
ChaedongIm <chaedong.im.dev@gmail.com>
2023-04-05 13:50:35 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    private long count = 0;
+    public long solution(int n) {
+        if (n <= 2) return n;
+        
+        long[] dp = new long[n+1];        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+
+        return dp[n] %1234567;
+    }
+}
