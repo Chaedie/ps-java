7b1816556d8482d731c0d51734af4664c1feedd3
ChaedongIm <chaedong.im.dev@gmail.com>
2023-04-12 10:58:06 +0900

    Update n＾2 배열 자르기.java

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,38 +1,19 @@
 import java.util.*;
 
 class Solution {
-    // nxn 2차원 배열생성
-    // i로 채우기
-    // copyOfRange
-    // 123456
-    // 223456
-    // 333456
-    // 444456
-    // 555556
-    // 666666
     public int[] solution(int n, long left, long right) {
-        // int[][] result = new int[n][n];
-        int[] answer = new int[(int)(right - left+1)];
-        
+        List<Integer> list = new ArrayList<>();
+        long len = (right - left + 1);
         long k = left - (left % n);
-        int l = 0;
         for (int i = (int) (left / n); i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int x = j;
-                if (x < i) x = i;          
-                // result[i][j] = x + 1;
-                
-                if (k >= left && k <= right) {
-                    answer[l++] = x+1;    
-                }
-                if (l == (int)(right - left+1) ) return answer;
+                int x = j < i ? i : j;      
+                if (k >= left && k <= right) list.add(x+1);
+                if (list.size() == len) return list.stream().mapToInt(num->num).toArray();
                 k++;
             }    
         }
         
-        // System.out.println(Arrays.deepToString(result));
-        // System.out.println(Arrays.toString(answer));
-
-        return answer;
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
