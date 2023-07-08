bd91cebab148a7e0de05ac02d47bd97e91ba1107
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-08 10:52:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.73 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 0.73 ms
+메모리: 67.5 MB, 시간: 0.73 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int sum = Arrays.stream(numbers).reduce((x,y) -> x + y).getAsInt();
-        int total = 45;
-        return (total - sum);
+        int[] candidates = new int[] {0,1,2,3,4,5,6,7,8,9};
+        for(int num : numbers) {
+            candidates[num] = 0;
+        }
+        
+        return Arrays.stream(candidates).reduce((acc,cur) -> acc+= cur).getAsInt();
     }
 }
